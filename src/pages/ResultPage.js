abe64c687af4737fc7bf7a3ab9989e4bd40c217b
dy6578ekdbs <dy6578ekdbs@gmail.com>
2022-11-22 12:46:11 +0900

    feat : result 페이지 data 뿌리기

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { TiDeleteOutline, TiArrowForwardOutline } from "react-icons/ti";
+import "../style/result.css";
 
 const ResultPage = ({ result }) => {
-  console.log("결과임!", result);
-  return <div>{result[1000].content}</div>;
+  const bestData = result[result.length - 1];
+
+  return (
+    <main>
+      <h1>TF-IDF</h1>
+
+      <h2>가장 비슷한 문장은</h2>
+      <h2 id="sentence">{bestData.content}</h2>
+      <h1 className="discription">분석 결과</h1>
+      <ul className="list">
+        {result.map((con, index) => {
+          if (index !== result.length - 1) {
+            return (
+              <li className="list__item">
+                <p className="num">{index + 1}. </p>
+                <p>{con.content}</p>
+                <p>=={con.distance}</p>
+              </li>
+            );
+          }
+        })}
+      </ul>
+      <div className="add">
+        <div className="add__item go">
+          <TiArrowForwardOutline id="go" />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default ResultPage;
