ddb288b30015ba74262a08f1b5a70bc32ab95979
dy6578ekdbs <dy6578ekdbs@gmail.com>
2022-12-05 15:42:37 +0900

    feat : 도메인 추가

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
     setFail(false);
 
-    fetch("http://127.0.0.1:5000/api/nlp", {
+    fetch("https://midubang.com/api/nlp", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
